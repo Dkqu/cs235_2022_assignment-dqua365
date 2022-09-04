96c7e00e6c5e180603eda6d383d9b7629e572509
NoGraphite <dqua365@aucklanduni.ac.nz>
2022-09-04 22:12:56 +1200

    First Changes: Integrating COVID web app into A2

--- music/__init__.py
+++ music/__init__.py
@@ -9,19 +9,15 @@ from music.domainmodel.track import Track
 
 # TODO: Access to the tracks should be implemented via the repository pattern and using blueprints, so this can not
 #  stay here!
-def create_some_track():
-    some_track = Track(1, "Heat Waves")
-    some_track.track_duration = 250
-    some_track.track_url = 'https://spotify/track/1'
-    return some_track
 
 def create_app():
+    # Create the Flask App OBJECT 
     app = Flask(__name__)
 
     @app.route('/')
     def home():
         some_track = create_some_track()
         # Use Jinja to customize a predefined html page rendering the layout for showing a single track.
-        return render_template('simple_track.html', track=some_track)
+        return render_template('simple_tr', track=some_track)
 
     return app
