653c43b8e0c7909a31293dcfed7513492e1d5f1f
NoGraphite <dqua365@aucklanduni.ac.nz>
2022-09-05 23:15:02 +1200

    Init changes

--- music/__init__.py
+++ music/__init__.py
@@ -1,7 +1,8 @@
 """Initialize Flask app."""
-
+from pathlib import Path
 from flask import Flask, render_template
-
+import covid.adapters.repository as repo
+from covid.adapters.memory_repository import MemoryRepository, populate
 # TODO: Access to the tracks should be implemented via the repository pattern and using blueprints, so this can not
 #  stay here!
 from music.domainmodel.track import Track
@@ -10,9 +11,24 @@ from music.domainmodel.track import Track
 # TODO: Access to the tracks should be implemented via the repository pattern and using blueprints, so this can not
 #  stay here!
 
-def create_app():
+def create_app(test_config=None):
     # Create the Flask App OBJECT 
     app = Flask(__name__)
+
+    # Configure the app from configuration-file settings.
+    app.config.from_object('config.Config')
+    # Data Path
+    data_path = Path('music') / 'adapters' / 'data'
+
+    if test_config is not None:
+        # Load test configuration, and override any configuration settings.
+        app.config.from_mapping(test_config)
+        data_path = app.config['TEST_DATA_PATH']
+
+    # Create the MemoryRepository implementation for a memory-based repository.
+    repo.repo_instance = MemoryRepository()
+    # fill the content of the repository from the provided csv files
+    populate(data_path, repo.repo_instance)
 
     @app.route('/')
     def home():
