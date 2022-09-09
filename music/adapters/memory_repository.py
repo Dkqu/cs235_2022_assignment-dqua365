e2401a750f40b04598b673a1326221e90a50c624
dqua365 <dqua365@aucklanduni.ac.nz>
2022-09-09 19:42:55 +1200

    Editing Memory Repository with methods

--- music/adapters/memory_repository.py
+++ music/adapters/memory_repository.py
@@ -1,17 +1,30 @@
+from ast import List
 from music.adapters.repository import AbstractRepository, RepositoryException
 from music.domainmodel.album import Album
 from music.domainmodel.user import User
 from music.domainmodel.track  import Track
 from music.domainmodel.review  import Review
+from music.domainmodel.artist import Artist
+from music.domainmodel.genre import Genre
+from music.domainmodel.playlist import PlayList
+
+
 
 class MemoryRepository(AbstractRepository):
     # Articles ordered by date, not id. id is assumed unique.
     def __init__(self):
+        #The following lists will be used to filter and access via ID
         self.__albums = list()
+        self.__albums_index = dict()
         self.__artists = list()
+        self.__artists_index = dict()
         self.__genres = list()
-        self.__users = list()
+        self.__genres_index = dict()
         self.__tracks = list()
+        self.__tracks_index = dict()
+
+        #Normal Data
+        self.__users = list()
         self.__playlists = list()
         self.__reviews = list()
 
@@ -21,16 +34,91 @@ class MemoryRepository(AbstractRepository):
     def get_user(self, user_name) -> User:
         return next((user for user in self.__users if user.user_name == user_name), None)
 
-    def add_album(self, album: Article):
-        insort_left(self.__articles, article)
-        self.__articles_index[article.id] = article
+    def add_playlist(self, playlist: PlayList):
+        self.__playlists.append(playlist)
+    def get_playlist(self, playlist) -> PlayList:
+        return next((pl for pl in self.__playlists if pl.list_of_tracks == playlist), None)
 
-    def get_article(self, id: int) -> Article:
-        article = None
+    def get_playlists_by_track(self, track: Track) -> List[PlayList]:
+
+        matching_playlists = list()
 
         try:
-            article = self.__articles_index[id]
+            for p in self.__playlists:
+                for t in p.list_of_tracks:
+                    if t.id == track.id:
+                        matching_playlists.append(p)
+                        break
+        except ValueError:
+            # No articles for specified date. Simply return an empty list.
+            pass
+
+        return matching_playlists
+    #Albums
+    def add_album(self, album: Album):
+        self.__albums.append(album)
+        self.__articles_index[album.id] = album
+
+    def get_album(self, id: int) -> Album:
+        album = None
+
+        try:
+            album = self.__albums_index[id]
         except KeyError:
             pass  # Ignore exception and return None.
 
-        return article
+        return album
+    #Artists
+    def add_artists(self, artist: Artist):
+        self.__artists.append(artist)
+        self.__artists_index[artist.id] = artist
+
+    def get_artist(self, id: int) -> Artist:
+        = None
+
+        try:
+            = self.__ _index[id]
+        except KeyError:
+            pass  # Ignore exception and return None.
+
+        return 
+
+    def get_artist(self, id: int) -> Artist:
+        artist = None
+
+        try:
+            artist= self.__artists_index[id]
+        except KeyError:
+            pass  # Ignore exception and return None.
+
+        return artist
+
+
+    def add_genre(self, genre: Genre):
+        self.__genres.append(genre)
+        self.__genres_index[genre.id] = genre
+
+    def get_genre(self, id: int) -> Genre:
+        genre = None
+
+        try:
+           genre = self.__genres_index[id]
+        except KeyError:
+            pass  # Ignore exception and return None.
+
+        return genre
+
+    def add_track(self, track: Track):
+        self.__tracks.append(track)
+        self.__tracks_index[track.id] = track
+
+
+    def get_track(self, id: int) -> Track:
+        track = None
+
+        try:
+            track = self.__tracks_index[id]
+        except KeyError:
+            pass  # Ignore exception and return None.
+
+        return track
