054c5480029b19b1dbeeb9660f908d2b5e90463a
NoGraphite <dqua365@aucklanduni.ac.nz>
2022-09-11 22:53:00 +1200

    Big changes - Repo

--- music/__init__.py
+++ music/__init__.py
@@ -1,8 +1,8 @@
 """Initialize Flask app."""
 from pathlib import Path
 from flask import Flask, render_template
-import covid.adapters.repository as repo
-from covid.adapters.memory_repository import MemoryRepository, populate
+import music.adapters.repository as repo
+from music.adapters.memory_repository import MemoryRepository, populate
 # TODO: Access to the tracks should be implemented via the repository pattern and using blueprints, so this can not
 #  stay here!
 from music.domainmodel.track import Track
@@ -26,14 +26,16 @@ def create_app(test_config=None):
         data_path = app.config['TEST_DATA_PATH']
 
     # Create the MemoryRepository implementation for a memory-based repository.
-    repo.repo_instance = MemoryRepository()
+    #repo.repo_instance = MemoryRepository()
     # fill the content of the repository from the provided csv files
-    populate(data_path, repo.repo_instance)
+    #populate(data_path, repo.repo_instance)
 
-    @app.route('/')
-    def home():
-        some_track = create_some_track()
-        # Use Jinja to customize a predefined html page rendering the layout for showing a single track.
-        return render_template('simple_tr', track=some_track)
+    with app.app_context():
+        # Register blueprints.
+        from .home import home
+        app.register_blueprint(home.home_blueprint)
+
+        from .home import home
+        app.register_blueprint(home.home_blueprint)
 
     return app
